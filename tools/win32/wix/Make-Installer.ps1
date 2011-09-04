4d366c54f4c7adadcd582559dc0c9d2e5b82105b
ForNeVeR <neverthness@gmail.com>
2011-09-04 21:34:20 +0700

    wix: More usable method for determining WiX toolset location.

--- tools/win32/wix/Make-Installer.ps1
+++ tools/win32/wix/Make-Installer.ps1
@@ -1,6 +1,12 @@
 # Configuration:
-$wixPath = 'c:\Program Files\Windows Installer XML v3.5\bin'
+
+# Path to WiX executables; your system is likely to accept this setting as is:
+$wixPath = "${Env:\WIX}\bin"
+
+# Path to directory with deployed LeechCraft:
 $sourcePath = '..\LeechCraft'
+
+# End of configuration.
 
 # Used tools:
 $candle = "$wixPath\candle"

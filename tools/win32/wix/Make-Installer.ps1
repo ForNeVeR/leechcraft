90233422219a85fb98bbce5631abe381e641d2fb
ForNeVeR <neverthness@gmail.com>
2011-09-04 16:10:01 +0700

    wix: Core binaries moved to another .wixlib fragment.

--- tools/win32/wix/Make-Installer.ps1
+++ tools/win32/wix/Make-Installer.ps1
@@ -7,5 +7,5 @@ $candle = "$wixPath\candle"
 $light = "$wixPath\light"
 
 # Create installer:
-& $candle leechcraft.wxs "-dSourcePath=`"$sourcePath`""
-& $light leechcraft.wixobj
+& $candle leechcraft.wxs *.wixlib "-dSourcePath=`"$sourcePath`""
+& $light *.wixobj -o leechcraft.msi
